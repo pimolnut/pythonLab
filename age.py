709ef27de4871386698ae8be6e2b07ce072f14a5
pimolnut <64010605@kmitl.ac.th>
2023-10-01 22:15:48 +0700

    age

---
+++ age.py
@@ -0,0 +1,9 @@
+name = input()
+year = int(input())
+age = 2021-year
+if age > 17:
+    print("Welcome",name,"to NongGipsy Pub")
+elif name == "A A":
+    print("Welcome", name, "to NongGipsy Pub")
+else:
+    print("You shall not pass!")
