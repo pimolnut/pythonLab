0131be5d4e849576a91a653f10bcc8f7a78c4805
pimolnut <64010605@kmitl.ac.th>
2023-10-01 21:56:44 +0700

    grade

---
+++ grade.py
@@ -0,0 +1,11 @@
+num = int(input())
+if num >= 80 and num <= 100:
+    print("A")
+elif num >= 70 and num <= 79:
+    print("B")
+elif num >= 60 and num <= 69:
+    print("C")
+elif num >= 50 and num <= 59:
+    print("D")
+else:
+    print("F")
